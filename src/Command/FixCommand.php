1e6796b4abcd4e53c761f7ee6a8c08b7b6797e80
Bruno <b7s@outlook.com>
2026-05-09 23:45:03 -0300

    refactor: remove unused `is_array` import from `FixCommand`

--- src/Command/FixCommand.php
+++ src/Command/FixCommand.php
@@ -13,7 +13,6 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Process\Process;
 
-use function is_array;
 use function sprintf;
 
 #[AsCommand(
